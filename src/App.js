67aaad01f6e31e97958285451b22c65d35617147
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-11-08 00:47:41 +0530

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
@@ -26,16 +26,13 @@ class App extends React.Component {
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       }
-      else{
-        setCurrentUser({userAuth});
-      }
+  
+        setCurrentUser(userAuth);
     });
   }
 
@@ -50,15 +47,22 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/'/>):(<SignInAndSignUpPage/>)} />
       </Switch> 
     </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(App);
